c9fba56d19b27b3a422409b7a4a71ca7dbd9ce9a
Yahya Kassab <ya7yakassab@gmail.com>
2023-02-04 23:32:14 +0200

    almost done

--- src/Components/Secondary/About Us Page/AboutUs.jsx
+++ src/Components/Secondary/About Us Page/AboutUs.jsx
@@ -1,5 +1,38 @@
-import React from "react"
+import { Button, TextField, Typography } from "@mui/material"
+import React, { useState } from "react"
 import Page from "../../Page"
 export default function AboutUs() {
-  return <Page nav={true}>aboutus</Page>
+  const [aboutUs, setAboutUs] = useState(
+    "aboutus aboutus aboutus aboutus aboutus aboutus  "
+  )
+  const [edit, setEdit] = useState(false)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(aboutUs)
+  }
+  return (
+    <Page nav={true}>
+      <Typography variant="h3" className="my-5">
+        About Us
+      </Typography>
+      <TextField
+        value={aboutUs}
+        disabled={!edit}
+        onChange={(e) => setAboutUs(e.target.value)}
+        fullWidth
+        multiline
+        rows={5}
+      />
+      <Button
+        variant="contained"
+        onClick={(e) => {
+          setEdit(!edit)
+          edit ? handleSubmit(e) : ""
+        }}
+        className="my-5 py-3 px-5"
+      >
+        {edit ? "Confirm" : "Edit"}
+      </Button>
+    </Page>
+  )
 }
--- src/Components/Secondary/Policies Page/Policies.jsx
+++ src/Components/Secondary/Policies Page/Policies.jsx
@@ -1,5 +1,38 @@
-import React from "react"
+import { Button, TextField, Typography } from "@mui/material"
+import React, { useState } from "react"
 import Page from "../../Page"
 export default function Policies() {
-  return <Page nav={true}>policies</Page>
+  const [policies, setPolicies] = useState(
+    "policies policies policies policies policies policies policies policies policies policies "
+  )
+  const [edit, setEdit] = useState(false)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(policies)
+  }
+  return (
+    <Page nav={true}>
+      <Typography variant="h3" className="my-5">
+        Policies
+      </Typography>
+      <TextField
+        value={policies}
+        disabled={!edit}
+        onChange={(e) => setPolicies(e.target.value)}
+        fullWidth
+        multiline
+        rows={5}
+      />
+      <Button
+        variant="contained"
+        onClick={(e) => {
+          setEdit(!edit)
+          edit ? handleSubmit(e) : ""
+        }}
+        className="my-5 py-3 px-5"
+      >
+        {edit ? "Confirm" : "Edit"}
+      </Button>
+    </Page>
+  )
 }
